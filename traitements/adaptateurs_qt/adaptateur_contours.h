523812678121d4f6193ab5a00a8679f7b9e0e236
Rimka-ui <kouroumakarinkan@gmail.com>
2025-11-24 21:11:10 +0000

    Declare functions for image processing and UI control
    Added function declarations for image conversion and slider control.

--- traitements/adaptateurs_qt/adaptateur_contours.h
+++ traitements/adaptateurs_qt/adaptateur_contours.h
@@ -2,3 +2,6 @@
 #define ADAPTATEUR_CONTOURS_H
 
 #endif // ADAPTATEUR_CONTOURS_H
+QPixmap CvMatToQPixmap(Mat matimage);// l'appel à cette fonction permettra de transformer notre image Mat en image Pixmap utilisable par Qt pour l'affichage dans un widget Qt
+void onSliderValueChanged_1(int value);// Cette fonction nous permettra de controler le niveau de contour avec un slider que l'on créer dans qt
+// nous utiliseront la fonction "connect"; pour faire un SIGNAL and SLOT sur la variable qui contrôle le contour dans l'appel à la fonction Canny de Opencv
